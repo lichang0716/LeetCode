35dda15de79dcc206745eb4b14da9a5712c0033c
lichang0716 <lichang.android@gmail.com>
2021-03-09 23:33:05 +0800

    shell sort update

--- BasicAlgorithm/SortArray/SortArray/main.swift
+++ BasicAlgorithm/SortArray/SortArray/main.swift
@@ -14,12 +14,12 @@ let array = [99, 5, 69, 33, 56, 13, 22, 55, 77, 48, 12, 88, 2, 69, 87]
 
 print(array)
 
-//print("bubble sort:\(bubbleSort(array))")
-//
-//print("selection sort:\(selectionSort(array))")
-//
-//print("quick sort:\(quickSort(array))")
-//
-//print("insertion sort:\(insertionSort(array))")
+print("bubble sort:\(bubbleSort(array))")
+
+print("selection sort:\(selectionSort(array))")
+
+print("quick sort:\(quickSort(array))")
+
+print("insertion sort:\(insertionSort(array))")
 
 print("shell sort:\(shellSort(array))")
